d7ba63fbb243b80181f0a6ebb418b47b3cb009c0
SK. Mohtasim Billah <polinaust87@gmail.com>
2019-05-21 01:36:53 +0600

    Adding files via upload

---
+++ ConsoleJsonParser/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using ConsoleJsonParser;
+
+namespace ConsoleJsonParser
+{
+    public static class Program
+    {
+        public static int keyLevel=0;
+        public static void Main(string[] args)
+        {
+            string myJson = "{\"key1\": 1,\"key2\": {\"key3\": 1,\"key4\": {\"key5\": 4}}}";
+            //{"key1": 1,"key2": {"key3": 1,"key4": {"key5": 4}}
+
+            ExpandoObject myExObj = myJson.SerializeJson();
+            
+            Console.ReadKey();
+        }
+
+        public static ExpandoObject SerializeJson(this string json)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            IDictionary<string, object> dictionary =
+                serializer.Deserialize<IDictionary<string, object>>(json);
+            return dictionary.Expando();
+        }
+
+        public static ExpandoObject Expando(this IDictionary<string, object> dictionary)
+        {
+            ExpandoObject expandoObject = new ExpandoObject();
+            IDictionary<string, object> objects = expandoObject;
+
+            foreach (var item in dictionary)
+            {
+                bool processed = false;
+
+                if (item.Value is IDictionary<string, object>)
+                {
+                    //keyLevel--;
+                    Console.Write(item.Key + ":");
+                    Console.WriteLine(keyLevel);
+                    objects.Add(item.Key, Expando((IDictionary<string, object>)item.Value));
+                    processed = true;
+                }
+                else if (item.Value is ICollection)
+                {
+                    List<object> itemList = new List<object>();
+
+                    foreach (var item2 in (ICollection)item.Value)
+
+                        if (item2 is IDictionary<string, object>)
+                            itemList.Add(Expando((IDictionary<string, object>)item2));
+                        else
+                            itemList.Add(Expando(new Dictionary<string,
+                                object> { { "Unknown", item2 } }));
+
+                    if (itemList.Count > 0)
+                    {
+                        objects.Add(item.Key, itemList);
+                        processed = true;
+                    }
+                }
+
+                if (!processed)
+                {
+                    keyLevel++;
+                    Console.Write(item.Key+":");
+                    Console.WriteLine(keyLevel);
+                    objects.Add(item);
+                }
+            }
+
+            return expandoObject;
+        }
+    }
+}
---
+++ LeastCommonAncestor/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeastCommonAncestor
+{
+    public class Node
+    {
+        public int data;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    public class Program
+    {
+        public Node root;
+
+        /* Function to find LCA of n1 and n2. The function assumes that both  
+           n1 and n2 are present in BST */
+        public virtual Node lca(Node node, int n1, int n2)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            // If both n1 and n2 are smaller than root, then LCA lies in left  
+            if (node.data > n1 && node.data > n2)
+            {
+                return lca(node.left, n1, n2);
+            }
+
+            // If both n1 and n2 are greater than root, then LCA lies in right  
+            if (node.data < n1 && node.data < n2)
+            {
+                return lca(node.right, n1, n2);
+            }
+
+            return node;
+        }
+
+        /* Driver program to test lca() */
+        public static void Main(string[] args)
+        {
+            // Let us construct the BST shown in the above figure  
+            Program tree = new Program();
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+            tree.root.left.right = new Node(5);
+            tree.root.right.left= new Node(6);
+            tree.root.right.left = new Node(7);
+            tree.root.left.left.left = new Node(8);
+            tree.root.left.left.right = new Node(9);
+
+            int n1 = 3, n2 = 7;
+            Node t = tree.lca(tree.root, n1, n2);
+            Console.WriteLine("LCA of " + n1 + " and " + n2 + " is " + t.data);
+
+            n1 = 3; n2 = 6;
+            t = tree.lca(tree.root, n1, n2);
+            Console.WriteLine("LCA of " + n1 + " and " + n2 + " is " + t.data);
+            Console.ReadKey();
+        }
+    }
+}
